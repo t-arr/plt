e1e1029473b62b5435a32daad7709d2fa71699e1
t-arr <123.123.123@123.123>
2024-10-31 17:19:50 +0200

    US3 - Test3

--- piglatin.py
+++ piglatin.py
@@ -7,7 +7,7 @@ class PigLatin:
         return self._phrase
 
     def translate(self) -> str:
-        vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'ä', 'ö']
+        vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'ä', 'ö', 'å']
         if self.get_phrase() == "":
             return "nil"
         last_character = self._phrase[-1]
@@ -15,4 +15,6 @@ class PigLatin:
             return self.get_phrase() + 'nay'
         if last_character in vowels:
             return self.get_phrase() + 'yay'
+        if last_character not in vowels:
+            return self.get_phrase() + 'ay'
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -20,3 +20,7 @@ class TestPigLatin(unittest.TestCase):
     def test_string_ends_with_vowel(self):
         translator = PigLatin("apple")
         self.assertEqual("appleyay", translator.translate())
+
+    def test_string_ends_with_consonant(self):
+        translator = PigLatin("ask")
+        self.assertEqual("askay", translator.translate())
